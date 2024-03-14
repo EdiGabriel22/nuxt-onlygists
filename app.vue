01c04d1266b484404dfa683f3c5a919d974a424b
edigabrielcontato@hotmail.com <104377342+EdiGabriel22@users.noreply.github.com>
2024-03-14 09:12:32 -0700

    chore: setup googleFonts
    - melhorias tailwindcss

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1 class="text-primary">OnlyGists</h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,12 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["nuxt-primevue", "@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
   css: ["primeicons/primeicons.css"],
+  googleFonts: {
+    base64: true,
+    fontsDir: "assests/fonts",
+    overwriting: true,
+    families: {
+      Inter: [300, 500, 800]
+    }
+  }
 })
