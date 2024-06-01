9442cc3d9ce2c507ebabae2ef0a3a186561e561d
edigabrielcontato@hotmail.com <104377342+EdiGabriel22@users.noreply.github.com>
2024-06-01 00:27:59 -0300

    feat: eslint

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h1 class="text-primary">OnlyGists</h1>
-    <Button label="Enviar"/>
-  </div>
+	<div>
+		<h1 class="text-primary">OnlyGists</h1>
+		<Button label="Enviar" />
+	</div>
 </template>
